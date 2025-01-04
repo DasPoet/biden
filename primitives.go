7d3a99221e555033766feac277addbb8799aa246
daspoet <cederk2306@gmail.com>
2025-01-04 21:16:08 +0100

    feat: fully implement primitives

---
+++ primitives.go
@@ -0,0 +1,389 @@
+package biden
+
+import "math"
+
+const (
+	BoolBytes = 1
+
+	UintBytes   = 8
+	Uint8Bytes  = 1
+	Uint16Bytes = 2
+	Uint32Bytes = 4
+	Uint64Bytes = 8
+
+	IntBytes   = 8
+	Int8Bytes  = 1
+	Int16Bytes = 2
+	Int32Bytes = 4
+	Int64Bytes = 8
+
+	Float32Bytes = 4
+	Float64Bytes = 8
+)
+
+type (
+	MarshalFunc[T any]   func(pos int, buf []byte, value T) int
+	UnmarshalFunc[T any] func(pos int, buf []byte) (T, int)
+)
+
+func Marshal[T any](value T, size int, marshaler MarshalFunc[T]) []byte {
+	buf := make([]byte, size)
+	marshaler(0, buf, value)
+	return buf
+}
+
+func Unmarshal[T any](buf []byte, unmarshaler UnmarshalFunc[T]) T {
+	value, _ := unmarshaler(0, buf)
+	return value
+}
+
+func MarshalBool(pos int, buf []byte, b bool) int {
+	if b {
+		buf[pos] = 1
+	} else {
+		buf[pos] = 0
+	}
+	return pos + BoolBytes
+}
+
+func UnmarshalBool(pos int, buf []byte) (bool, int) {
+	raw := buf[pos]
+	return raw == 1, pos + BoolBytes
+}
+
+func MarshalUint8(pos int, buf []byte, n uint8) int {
+	buf[pos] = byte(n)
+	return pos + Uint8Bytes
+}
+
+func UnmarshalUint8(pos int, buf []byte) (uint8, int) {
+	raw := buf[pos]
+	return uint8(raw), pos + Uint8Bytes
+}
+
+func MarshalUint16(pos int, buf []byte, n uint16) int {
+	window := buf[pos : pos+Uint16Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[1]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+
+	return pos + Uint16Bytes
+}
+
+func UnmarshalUint16(pos int, buf []byte) (uint16, int) {
+	window := buf[pos : pos+Uint16Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[1]
+
+	lo := uint16(window[0])
+	hi := uint16(window[1]) << 8
+
+	return lo | hi, pos + Uint16Bytes
+}
+
+func MarshalUint32(pos int, buf []byte, n uint32) int {
+	window := buf[pos : pos+Uint32Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[3]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+	window[2] = byte(n >> 16)
+	window[3] = byte(n >> 24)
+
+	return pos + Uint32Bytes
+}
+
+func UnmarshalUint32(pos int, buf []byte) (uint32, int) {
+	window := buf[pos : pos+Uint32Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[3]
+
+	lo := uint32(window[0]) | uint32(window[1])<<8
+	hi := uint32(window[2])<<16 | uint32(window[3])<<24
+
+	return lo | hi, pos + Uint32Bytes
+}
+
+func MarshalUint64(pos int, buf []byte, n uint64) int {
+	window := buf[pos : pos+Uint64Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[7]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+	window[2] = byte(n >> 16)
+	window[3] = byte(n >> 24)
+	window[4] = byte(n >> 32)
+	window[5] = byte(n >> 40)
+	window[6] = byte(n >> 48)
+	window[7] = byte(n >> 56)
+
+	return pos + Uint64Bytes
+}
+
+func UnmarshalUint64(pos int, buf []byte) (uint64, int) {
+	window := buf[pos : pos+Uint64Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[7]
+
+	lo := uint64(window[0]) | uint64(window[1])<<8 | uint64(window[2])<<16 | uint64(window[3])<<24
+	hi := uint64(window[4])<<32 | uint64(window[5])<<40 | uint64(window[6])<<48 | uint64(window[7])<<56
+
+	return lo | hi, pos + Uint64Bytes
+}
+
+func MarshalUint(pos int, buf []byte, n uint) int {
+	window := buf[pos : pos+UintBytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[7]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+	window[2] = byte(n >> 16)
+	window[3] = byte(n >> 24)
+	window[4] = byte(n >> 32)
+	window[5] = byte(n >> 40)
+	window[6] = byte(n >> 48)
+	window[7] = byte(n >> 56)
+
+	return pos + UintBytes
+}
+
+func UnmarshalUint(pos int, buf []byte) (uint, int) {
+	window := buf[pos : pos+Uint64Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[7]
+
+	lo := uint(window[0]) | uint(window[1])<<8 | uint(window[2])<<16 | uint(window[3])<<24
+	hi := uint(window[4])<<32 | uint(window[5])<<40 | uint(window[6])<<48 | uint(window[7])<<56
+
+	return lo | hi, pos + UintBytes
+}
+
+func MarshalInt8(pos int, buf []byte, n int8) int {
+	buf[pos] = byte(n)
+	return pos + Int8Bytes
+}
+
+func UnmarshalInt8(pos int, buf []byte) (int8, int) {
+	raw := buf[pos]
+	return int8(raw), pos + Int8Bytes
+}
+
+func MarshalInt16(pos int, buf []byte, n int16) int {
+	window := buf[pos : pos+Int16Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[1]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+
+	return pos + Int16Bytes
+}
+
+func UnmarshalInt16(pos int, buf []byte) (int16, int) {
+	window := buf[pos : pos+Int16Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[1]
+
+	lo := int16(window[0])
+	hi := int16(window[1]) << 8
+
+	return lo | hi, pos + Int16Bytes
+}
+
+func MarshalInt32(pos int, buf []byte, n int32) int {
+	window := buf[pos : pos+Int32Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[3]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+	window[2] = byte(n >> 16)
+	window[3] = byte(n >> 24)
+
+	return pos + Int32Bytes
+}
+
+func UnmarshalInt32(pos int, buf []byte) (int32, int) {
+	window := buf[pos : pos+Int32Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[3]
+
+	lo := int32(window[0]) | int32(window[1])<<8
+	hi := int32(window[2])<<16 | int32(window[3])<<24
+
+	return lo | hi, pos + Int32Bytes
+}
+
+func MarshalInt64(pos int, buf []byte, n int64) int {
+	window := buf[pos : pos+Int64Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[7]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+	window[2] = byte(n >> 16)
+	window[3] = byte(n >> 24)
+	window[4] = byte(n >> 32)
+	window[5] = byte(n >> 40)
+	window[6] = byte(n >> 48)
+	window[7] = byte(n >> 56)
+
+	return pos + Int64Bytes
+}
+
+func UnmarshalInt64(pos int, buf []byte) (int64, int) {
+	window := buf[pos : pos+Int64Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[7]
+
+	lo := int64(window[0]) | int64(window[1])<<8 | int64(window[2])<<16 | int64(window[3])<<24
+	hi := int64(window[4])<<32 | int64(window[5])<<40 | int64(window[6])<<48 | int64(window[7])<<56
+
+	return lo | hi, pos + Int64Bytes
+}
+
+func MarshalFloat32(pos int, buf []byte, n float32) int {
+	bits := math.Float32bits(n)
+
+	window := buf[pos : pos+Float32Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[3]
+
+	window[0] = byte(bits)
+	window[1] = byte(bits >> 8)
+	window[2] = byte(bits >> 16)
+	window[3] = byte(bits >> 24)
+
+	return pos + Float32Bytes
+}
+
+func UnmarshalFloat32(pos int, buf []byte) (float32, int) {
+	window := buf[pos : pos+Float32Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[3]
+
+	lo := uint32(window[0]) | uint32(window[1])<<8
+	hi := uint32(window[2])<<16 | uint32(window[3])<<24
+
+	return math.Float32frombits(lo | hi), pos + Float32Bytes
+}
+
+func MarshalFloat64(pos int, buf []byte, n float64) int {
+	bits := math.Float64bits(n)
+
+	window := buf[pos : pos+Float64Bytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[7]
+
+	window[0] = byte(bits)
+	window[1] = byte(bits >> 8)
+	window[2] = byte(bits >> 16)
+	window[3] = byte(bits >> 24)
+	window[4] = byte(bits >> 32)
+	window[5] = byte(bits >> 40)
+	window[6] = byte(bits >> 48)
+	window[7] = byte(bits >> 56)
+
+	return pos + Float64Bytes
+}
+
+func UnmarshalFloat64(pos int, buf []byte) (float64, int) {
+	window := buf[pos : pos+Int64Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[7]
+
+	lo := uint64(window[0]) | uint64(window[1])<<8 | uint64(window[2])<<16 | uint64(window[3])<<24
+	hi := uint64(window[4])<<32 | uint64(window[5])<<40 | uint64(window[6])<<48 | uint64(window[7])<<56
+
+	return math.Float64frombits(lo | hi), pos + Int64Bytes
+}
+
+func MarshalInt(pos int, buf []byte, n int) int {
+	window := buf[pos : pos+IntBytes]
+
+	// early bounds check to guarantee safety of
+	// writes below and to increase performance
+	_ = window[7]
+
+	window[0] = byte(n)
+	window[1] = byte(n >> 8)
+	window[2] = byte(n >> 16)
+	window[3] = byte(n >> 24)
+	window[4] = byte(n >> 32)
+	window[5] = byte(n >> 40)
+	window[6] = byte(n >> 48)
+	window[7] = byte(n >> 56)
+
+	return pos + IntBytes
+}
+
+func UnmarshalInt(pos int, buf []byte) (int, int) {
+	window := buf[pos : pos+Int64Bytes]
+
+	// early bounds check to guarantee safety of
+	// reads below and to increase performance
+	_ = window[7]
+
+	lo := int(window[0]) | int(window[1])<<8 | int(window[2])<<16 | int(window[3])<<24
+	hi := int(window[4])<<32 | int(window[5])<<40 | int(window[6])<<48 | int(window[7])<<56
+
+	return lo | hi, pos + IntBytes
+}
+
+func StringBytes(s string) int {
+	return IntBytes + len(s)
+}
+
+func MarshalString(pos int, buf []byte, s string) int {
+	pos = MarshalInt(pos, buf, len(s))
+	copy(buf[pos:], s)
+
+	return pos + len(s)
+}
+
+func UnmarshalString(pos int, buf []byte) (string, int) {
+	len, pos := UnmarshalInt(pos, buf)
+
+	return string(buf[pos : pos+len]), pos + len
+}
---
+++ primitives_test.go
@@ -0,0 +1,205 @@
+package biden
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshalUnmarshalUint8(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Uint8Bytes, MarshalUint8, UnmarshalUint8)
+	testMarshalUnmarshal(t, 1, Uint8Bytes, MarshalUint8, UnmarshalUint8)
+	testMarshalUnmarshal(t, 10, Uint8Bytes, MarshalUint8, UnmarshalUint8)
+	testMarshalUnmarshal(t, 255, Uint8Bytes, MarshalUint8, UnmarshalUint8)
+}
+
+func TestMarshalUnmarshalUint16(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Uint16Bytes, MarshalUint16, UnmarshalUint16)
+	testMarshalUnmarshal(t, 1, Uint16Bytes, MarshalUint16, UnmarshalUint16)
+	testMarshalUnmarshal(t, 10, Uint16Bytes, MarshalUint16, UnmarshalUint16)
+	testMarshalUnmarshal(t, 255, Uint16Bytes, MarshalUint16, UnmarshalUint16)
+	testMarshalUnmarshal(t, 256, Uint16Bytes, MarshalUint16, UnmarshalUint16)
+	testMarshalUnmarshal(t, 1000, Uint16Bytes, MarshalUint16, UnmarshalUint16)
+}
+
+func TestMarshalUnmarshalUint32(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+	testMarshalUnmarshal(t, 1, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+	testMarshalUnmarshal(t, 10, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+	testMarshalUnmarshal(t, 255, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+	testMarshalUnmarshal(t, 256, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+	testMarshalUnmarshal(t, 1000, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+	testMarshalUnmarshal(t, 25_000, Uint32Bytes, MarshalUint32, UnmarshalUint32)
+}
+
+func TestMarshalUnmarshalUint64(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 1, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 10, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 255, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 256, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 1000, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 25_000, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+	testMarshalUnmarshal(t, 100_000, Uint64Bytes, MarshalUint64, UnmarshalUint64)
+}
+
+func TestMarshalUnmarshalUint(t *testing.T) {
+	testMarshalUnmarshal(t, 0, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 1, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 10, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 255, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 256, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 1000, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 25_000, UintBytes, MarshalUint, UnmarshalUint)
+	testMarshalUnmarshal(t, 100_000, UintBytes, MarshalUint, UnmarshalUint)
+}
+
+func TestMarshalUnmarshalInt8(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Int8Bytes, MarshalInt8, UnmarshalInt8)
+	testMarshalUnmarshal(t, 1, Int8Bytes, MarshalInt8, UnmarshalInt8)
+	testMarshalUnmarshal(t, 10, Int8Bytes, MarshalInt8, UnmarshalInt8)
+	testMarshalUnmarshal(t, 127, Int8Bytes, MarshalInt8, UnmarshalInt8)
+
+	testMarshalUnmarshal(t, -1, Int8Bytes, MarshalInt8, UnmarshalInt8)
+	testMarshalUnmarshal(t, -10, Int8Bytes, MarshalInt8, UnmarshalInt8)
+	testMarshalUnmarshal(t, -128, Int8Bytes, MarshalInt8, UnmarshalInt8)
+}
+
+func TestMarshalUnmarshalInt16(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, 1, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, 10, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, 255, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, 256, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, 1000, Int16Bytes, MarshalInt16, UnmarshalInt16)
+
+	testMarshalUnmarshal(t, -1, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, -10, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, -255, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, -256, Int16Bytes, MarshalInt16, UnmarshalInt16)
+	testMarshalUnmarshal(t, -1000, Int16Bytes, MarshalInt16, UnmarshalInt16)
+}
+
+func TestMarshalUnmarshalInt32(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, 1, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, 10, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, 255, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, 256, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, 1000, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, 25_000, Int32Bytes, MarshalInt32, UnmarshalInt32)
+
+	testMarshalUnmarshal(t, -1, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, -10, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, -255, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, -256, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, -1000, Int32Bytes, MarshalInt32, UnmarshalInt32)
+	testMarshalUnmarshal(t, -25_000, Int32Bytes, MarshalInt32, UnmarshalInt32)
+}
+
+func TestMarshalUnmarshalInt64(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 1, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 10, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 255, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 256, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 1000, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 25_000, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, 100_000, Int64Bytes, MarshalInt64, UnmarshalInt64)
+
+	testMarshalUnmarshal(t, -1, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, -10, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, -255, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, -256, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, -1000, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, -25_000, Int64Bytes, MarshalInt64, UnmarshalInt64)
+	testMarshalUnmarshal(t, -100_000, Int64Bytes, MarshalInt64, UnmarshalInt64)
+}
+
+func TestMarshalUnmarshalInt(t *testing.T) {
+	testMarshalUnmarshal(t, 0, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 1, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 10, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 255, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 256, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 1000, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 25_000, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, 100_000, IntBytes, MarshalInt, UnmarshalInt)
+
+	testMarshalUnmarshal(t, -1, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, -10, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, -255, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, -256, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, -1000, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, -25_000, IntBytes, MarshalInt, UnmarshalInt)
+	testMarshalUnmarshal(t, -100_000, IntBytes, MarshalInt, UnmarshalInt)
+}
+
+func TestMarshalUnmarshalFloat32(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 1, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 10, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 255, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 256, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 1000, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 25_000, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 100_000, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+
+	testMarshalUnmarshal(t, -1, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -10, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -255, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -256, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -1000, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -25_000, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -100_000, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+
+	testMarshalUnmarshal(t, 1.12, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -10.04, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 255.09, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -256.99, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 1000.56, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, -25_000.33, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+	testMarshalUnmarshal(t, 100_000.19, Float32Bytes, MarshalFloat32, UnmarshalFloat32)
+}
+
+func TestMarshalUnmarshalFloat64(t *testing.T) {
+	testMarshalUnmarshal(t, 0, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 1, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 10, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 255, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 256, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 1000, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 25_000, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 100_000, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+
+	testMarshalUnmarshal(t, -1, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -10, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -255, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -256, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -1000, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -25_000, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -100_000, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+
+	testMarshalUnmarshal(t, 1.12, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -10.04, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 255.09, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -256.99, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 1000.56, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, -25_000.33, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+	testMarshalUnmarshal(t, 100_000.19, Float64Bytes, MarshalFloat64, UnmarshalFloat64)
+}
+
+func TestMarshalUnmarshalString(t *testing.T) {
+	testMarshalUnmarshal(t, "", StringBytes(""), MarshalString, UnmarshalString)
+	testMarshalUnmarshal(t, "hello world", StringBytes("hello world"), MarshalString, UnmarshalString)
+}
+
+func testMarshalUnmarshal[T any](t *testing.T, value T, size int, marshaler MarshalFunc[T], unmarshaler UnmarshalFunc[T]) {
+	encoded := Marshal(value, size, marshaler)
+	decoded := Unmarshal(encoded, unmarshaler)
+
+	assert.Equal(t, value, decoded)
+
+	encoded2 := Marshal(decoded, size, marshaler)
+	assert.Equal(t, encoded, encoded2)
+}

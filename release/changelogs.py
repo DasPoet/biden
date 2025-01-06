c767ce2bcdd325655c04e390d5c17426e658a295
daspoet <cederk2306@gmail.com>
2025-01-06 01:14:16 +0100

    ci: setup release job

---
+++ release/changelogs.py
@@ -0,0 +1,89 @@
+import re
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Changelist:
+    type: str
+    changes: list[str] = field(default_factory=list)
+
+
+@dataclass
+class Version:
+    name: str
+    changelists: list[Changelist] = field(default_factory=list)
+
+    def to_markdown(self) -> str:
+        changes = [f"## {cl.type}\n\n{'\n'.join('- ' + change for change in cl.changes)}" for cl in self.changelists]
+
+        return "# Changes\n\n" + "\n\n".join(changes)
+
+@dataclass
+class Changelog:
+    versions: list[Version] = field(default_factory=list)
+
+
+def parse_version_line(line: str) -> str | None:
+    pattern = re.compile(r"## \[(?P<version>\d+\.\d+\.\d+)] - \d\d\d\d-\d\d-\d\d")
+
+    match = pattern.match(line)
+    if not match:
+        return None
+
+    return match.group("version")
+
+
+def parse_changes_line(line: str) -> str | None:
+    pattern = re.compile(r"### (?P<type>Added|Changed|Deprecated|Removed|Fixed|Security)")
+
+    match = pattern.match(line)
+    if not match:
+        return None
+
+    return match.group("type")
+    
+
+def parse_change_line(line: str) -> str | None:
+    if line.startswith("* "):
+        return line.removeprefix("* ")
+
+    if line.startswith("- "):
+        return line.removeprefix("- ")
+
+    return None
+
+
+def parse_changelog(content: str) -> Changelog:
+    versions: list[Version] = []
+    changelists_by_type: dict[str, Changelist] = {}
+
+    last_version: Version | None = None
+    last_type: str | None = None
+
+    for line in content.splitlines():
+        version = parse_version_line(line)
+        if version is not None:
+            if last_version is not None:
+                last_version.changelists = [cl for cl in changelists_by_type.values() if len(cl.changes) > 0]
+                versions.append(last_version)
+
+            last_version = Version(name=version)
+
+        changes_type = parse_changes_line(line)
+        if changes_type is not None:
+            last_type = changes_type.lower()
+            changelists_by_type[last_type] = Changelist(type=changes_type)
+
+        change = parse_change_line(line)
+        if change is not None:
+            assert last_type, line
+            assert last_version, line
+
+            changelists_by_type[last_type].changes.append(change)
+    
+    if last_version is not None:
+        last_version.changelists = [cl for cl in changelists_by_type.values() if len(cl.changes) > 0]
+        versions.append(last_version)
+
+    return Changelog(versions)
---
+++ release/create-tags.py
@@ -0,0 +1,28 @@
+import git
+
+from sys import argv
+from changelogs import parse_changelog
+
+
+def create_git_tag_if_not_exists(name: str, description: str | None = None):
+    repo = git.Repo(".")
+
+    if name not in [tag.name for tag in repo.tags]:
+        print(f"creating tag '{name}'")
+        repo.create_tag(name, message=description)
+    else:
+        print(f"skipping tag '{name}', because it already exists")
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print(f"usage: python {argv[0]} <CHANGELOG.md>")
+        exit(1)
+
+    with open(argv[1], "r") as file:
+        changelog = parse_changelog(file.read())
+
+    for version in changelog.versions:
+        description = version.to_markdown()
+
+        create_git_tag_if_not_exists(name=version.name, description=f"Version {version.name}")
